c158f86ad8361e0aed214e8281813b22d3e21115
Maksiosmf <136759557+maksiosmf@users.noreply.github.com>
2026-03-06 14:28:16 +0100

    3'rd key fixes

--- src/products/create.product.dto.ts
+++ src/products/create.product.dto.ts
@@ -1,11 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @Type(() => Number)
   @IsNumber()
   @Min(0.01)
   price: number;
